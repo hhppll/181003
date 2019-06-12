c8ab094e994f360fd536f52baf5a4448c0a840a8
hhppll <50311644+hhppll@users.noreply.github.com>
2019-06-12 20:16:24 +0800

    Update TreeController.java

--- src/main/java/com/jk/controller/TreeController.java
+++ src/main/java/com/jk/controller/TreeController.java
@@ -30,7 +30,7 @@ public class TreeController {
     private TreeService treeService;
     @RequestMapping("main")
     public String main() {
-        System.out.println("到主页面3");
+        System.out.println("到主页面5");
         return "main";
     }
     /**
